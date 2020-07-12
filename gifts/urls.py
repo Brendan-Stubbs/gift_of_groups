279cc494f221830e086f346f4ff982c80fc1115f
Brendan Stubbs <stubbsbrendan@gmail.com>
2020-07-12 23:08:58 +0200

    group view and admin grant

--- gifts/urls.py
+++ gifts/urls.py
@@ -6,4 +6,14 @@ urlpatterns = [
     path("new_group/", views.CreateGroup.as_view(), name="new_group"),
     path("groups", views.ViewGroups.as_view(), name="view_groups"),
     path("edit_group/<int:id>/", views.EditGroup.as_view(), name="edit_group"),
+    path(
+        "groups/<int:id>/",
+        views.ViewIndividualGroup.as_view(),
+        name="view_individual_group",
+    ),
+    path(
+        "group_grant_admin/<int:group_id>/<int:profile_id>/",
+        views.GrantAdminAccess.as_view(),
+        name="group_grant_admin",
+    ),
 ]
