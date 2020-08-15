81bb91142f7046e33537919a475f2c155bcc7820
Brendan Stubbs <stubbsbrendan@gmail.com>
2020-08-15 17:49:10 +0200

    switch to mysql

--- gift_of_groups/settings.py
+++ gift_of_groups/settings.py
@@ -103,12 +103,7 @@ DATABASES = {
 }
 
 if ENVIRONMENT == "staging":
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        DATABASES = local_settings.DATABASES
 
 
 # Password validation
