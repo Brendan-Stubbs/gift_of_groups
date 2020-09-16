697d432fdd6e5936d54e4ef60a6127be2635af74
Brendan Stubbs <stubbsbrendan@gmail.com>
2020-09-16 21:28:30 +0200

    calendar logic

--- gifts/static/gifts/js/calendar.js
+++ gifts/static/gifts/js/calendar.js
@@ -8,6 +8,7 @@ function increment_month() {
         $('#monthName').text(monthNames[++current_displayed_month%12]);
         months_from_now ++;
         check_buttons()
+        populate_calendar_with_month(current_displayed_month);
     }
 }
 
@@ -16,6 +17,7 @@ function decrement_month() {
         $('#monthName').text(monthNames[--current_displayed_month%12]);
         months_from_now --;
         check_buttons();
+        populate_calendar_with_month(current_displayed_month);
     }
 }
 
@@ -55,8 +57,35 @@ function populate_calendar_with_month(month) {
     if (month > 11){year_to_display ++;}
     month = month % 11
     let total_days = new Date(year_to_display, month + 1, 0).getDate()
-    let starting_weekday = new Date(year_to_display, month, 1).getDay()
-    let calendarContent = ""
+    let current_weekday = new Date(year_to_display, month, 1).getDay()
+    let current_day = 1
+    let calendarContent = "<tr><td>S</td><td>M</td><td>T</td><td>W</td><td>T</td><td>F</td><td>S</td></tr>"
+    let day_countdown = current_weekday;
+
+    while(day_countdown-- > 0){
+        calendarContent += '<td class="empty-day"></td>'
+    };
+
+    while(current_day <= total_days){
+        if(current_weekday > 6){
+            current_weekday = 0;
+        };
+
+        if (current_weekday===0 && current_day < total_days+1 && current_day !== 1){
+            calendarContent += "<tr>";
+        };
+
+        calendarContent += `<td>${current_day}</td>`;
+
+        current_day++;
+        current_weekday++;
+    };
+
+    if (current_day!=0){
+        calendarContent += '</tr>'
+    };
+    $('#calendarBody').empty();
+    $('#calendarBody').append(calendarContent);
 }
 
 $(document).ready(function () {
