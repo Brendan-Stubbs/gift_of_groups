9f5a8128172996f07456db1e73b9e2183d56238e
Brendan-Stubbs <brendan@root.co.za>
2023-01-22 09:18:02 +0200

    WIP

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9
+
+WORKDIR '/app'
+
+COPY '/requirements.txt' .
+RUN pip3 install -r requirements.txt --no-cache-dir
+COPY . . 
+
+CMD ["python","manage.py", "runserver", "0.0.0.0:8090"]
--- gift_of_groups/settings.py
+++ gift_of_groups/settings.py
@@ -98,8 +98,14 @@ try:
 except:
     DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'development',
+          'USER': 'giftly',
+          'PASSWORD': 'pass',
+          'HOST': 'mysql',
+          'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         }
     }
 
